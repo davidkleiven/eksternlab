6a0eafb4be640e8a4cade8c94bd3df6af48ed3d3
David Kleiven <davidkleiven446@gmail.com>
2019-08-19 09:37:14 +0200

    Set name

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
+    name='eksternlab',
     version=1.0,
     author='David Kleiven',
-    email='david.kleiven@ntnu.no'
+    email='david.kleiven@ntnu.no',
     packages=find_packages()
 )
