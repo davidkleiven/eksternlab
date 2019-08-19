e21e1d8b92a934e412db2eb3bfaafd894842fe17
David Kleiven <davidkleiven446@gmail.com>
2019-08-19 13:40:39 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -2,8 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='eksternlab',
+    description='Useful scripts for NTNU lab for engineers',
     version=1.0,
     author='David Kleiven',
-    email='david.kleiven@ntnu.no',
-    packages=find_packages()
+    author_email='david.kleiven@ntnu.no',
+    packages=find_packages(),
+    classifiers=[
+        'Development Stats :: 4 - Beta',
+        'Intended Audience :: Students',
+        'Topic :: Data analysis :: Post processing Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 2.7'
+    ]
 )
