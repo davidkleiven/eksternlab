458c3db85f8afa8bef3cd248cb19409d6b3a2421
David Kleiven <davidkleiven446@gmail.com>
2019-08-19 13:12:17 +0200

    Add extend data function

--- test/test_lane.py
+++ test/test_lane.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 from eksternlab import Lane
+from eksternlab import extend_dataset
 
 
 class TestLane(unittest.TestCase):
@@ -19,6 +20,17 @@ class TestLane(unittest.TestCase):
 
         radius = lane.radius_of_curvature(smooth=5)
         self.assertTrue(np.allclose(radius(x)[10:-10], -1.0, atol=0.1))
+
+    def test_extend_dataset(self):
+        x = np.linspace(0.0, 2.0, 100)
+        y = 2*x
+
+        x_new, y_new = extend_dataset(x, y, -1.0, 4.0, num_points=10)
+        self.assertAlmostEqual(x_new[0], -1.0)
+        self.assertAlmostEqual(x_new[-1], 4.0)
+        self.assertAlmostEqual(y_new[0], 2*x_new[0])
+        self.assertAlmostEqual(y_new[-1], 2*x_new[-1])
+
 
 if __name__ == '__main__':
     unittest.main()
